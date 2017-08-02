049177c0960f770c0152ac3d32530e75fc5ec6ab
Bill Dueber <bill@dueber.com>
2017-08-02 11:51:07 -0400

    Update for new salary website structure

--- get_salaries.rb
+++ get_salaries.rb
@@ -14,20 +14,33 @@ http://www.umsalary.info/deptsearch.php?Dept=Library+Info+Tech-+LTIG&Year=0&Camp
 ]
 
 
-def getem(url)
-  doc = Nokogiri::HTML(open(url).read)
-  salary_table =  doc.css('table')[2]
+
+
+
+def get_url(url)
+  getem(open(url).read)
+end
+
+def getem(fh)
+  doc = Nokogiri::HTML(fh.read)
+  salary_table =  doc.css('table.index')
   salary_table.css('tr').each do |row|
     first_cell = row.css('td').first
     next unless first_cell # must be a header
     next if first_cell.css('a').empty?
-    name = first_cell.css('a').text.strip
+    name = first_cell.css('a').text.strip.gsub(/ ,/, ', ')
     raw_salary = row.css('td')[-2].text.strip
     salary = raw_salary.gsub(/[^\d\.]/, '').to_f
-    puts [name, salary.to_s, row.css('td')[-3].text.strip].join("\t")
+    dept = row.css('td')[2].text.gsub(/Library\s*Info\s*Tech\s*\-\s*/, '')
+    puts [name, salary.to_s, dept].join("\t")
   end
 end
 
-urls.each do |url|
-  getem(url)
-end
+
+u1 = "http://www.umsalary.info/deptsearch.php?Dept=Library+Info%25&Year=0&Campus=1"
+u2 = "http://www.umsalary.info/deptsearch.php?Dept=LibraryInfo%25&Year=0&Campus=1"
+u3 = "http://www.umsalary.info/deptsearch.php?Dept=Library+Dean+-+General&Year=0&Campus=1"
+getem open(u1)
+getem open(u2)
+getem open(u3)
+
